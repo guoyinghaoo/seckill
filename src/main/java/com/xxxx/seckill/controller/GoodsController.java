2ee443b5801d212fabae5120b97685bd9d05dc10
gyh <806029224@qq.com>
2021-09-19 12:28:43 +0800

    点击秒杀库存减1.订单表更新

--- src/main/java/com/xxxx/seckill/controller/GoodsController.java
+++ src/main/java/com/xxxx/seckill/controller/GoodsController.java
@@ -3,9 +3,7 @@ package com.xxxx.seckill.controller;
 import com.xxxx.seckill.pojo.User;
 import com.xxxx.seckill.service.IGoodsService;
 import com.xxxx.seckill.service.IUserService;
-import com.xxxx.seckill.vo.DetailVo;
 import com.xxxx.seckill.vo.GoodsVo;
-import com.xxxx.seckill.vo.RespBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -14,14 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.context.WebContext;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 商品
@@ -54,14 +49,14 @@ public class GoodsController {
 	 * @since: 1.0.0
 	 * @Author: zhoubin
 	 */
-	@RequestMapping(value = "/toList")
-	public String toList(Model model, User user) {
+	@RequestMapping(value = "/toListnext")
+	public String toListnext(Model model, User user) {
 		//Redis中获取页面，如果不为空，直接返回页面
-		/*ValueOperations valueOperations = redisTemplate.opsForValue();
+		ValueOperations valueOperations = redisTemplate.opsForValue();
 		String html = (String) valueOperations.get("goodsList");
 		if (!StringUtils.isEmpty(html)) {
 			return html;
-		}*/
+		}
 		model.addAttribute("user", user);
 		model.addAttribute("goodsList", goodsService.findGoodsVo());
 		// return "goodsList";
@@ -76,6 +71,26 @@ public class GoodsController {
 
 	}
 
+	/**
+	 * 功能描述: 跳转商品列表页
+	 * windows优化前QPS：1332
+	 * 缓存QPS：2342
+	 *
+	 * @param:
+	 * @return: 乐字节：专注线上IT培训
+	 * 答疑老师微信：lezijie
+	 * @since: 1.0.0
+	 * @Author: zhoubin
+	 */
+	@RequestMapping(value = "/toList")
+	public String toList(Model model, User user) {
+
+		model.addAttribute("user", user);
+		model.addAttribute("goodsList", goodsService.findGoodsVo());
+		return "goodsList";
+	}
+
+
 
 	/**
 	 * 功能描述: 跳转商品详情页
@@ -88,7 +103,7 @@ public class GoodsController {
 	 */
 	@RequestMapping(value = "/detail/{goodsId}")
 	public String toDetail2(Model model, User user, @PathVariable Long goodsId,
-	                        HttpServletRequest request, HttpServletResponse response) {
+							HttpServletRequest request, HttpServletResponse response) {
 		ValueOperations valueOperations = redisTemplate.opsForValue();
 		//Redis中获取页面，如果不为空，直接返回页面
 		String html = (String) valueOperations.get("goodsDetail:" + goodsId);
@@ -140,7 +155,7 @@ public class GoodsController {
 	 * @since: 1.0.0
 	 * @Author:zhoubin
 	 */
-/*	@RequestMapping("/detail/{goodsId}")
+	/*@RequestMapping("/detail/{goodsId}")
 	@ResponseBody
 	public RespBean toDetail(User user, @PathVariable Long goodsId) {
 		GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
